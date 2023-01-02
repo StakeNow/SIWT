1aaf73f48b7ff8459147683e9e69279be50aa927
Roy <hi@royscheeren.com>
2023-01-02 12:24:50 +0100

    refactor: take out unused commands code

--- packages/discord/src/main.ts
+++ packages/discord/src/main.ts
@@ -6,23 +6,20 @@
 
 import { Client, REST } from 'discord.js'
 import { Database } from 'sqlite3'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 import { db, initDb } from './app/common/database'
 import { client, rest } from './app/common/discord'
 import { startApi } from './app/modules/api/api'
-import { install as installCommands, load as loadCommands } from './app/modules/commands'
 import { load as loadEvents } from './app/modules/events'
+
 
 client.login(process.env.DISCORD_TOKEN || '')
 
 const startBot = ({ client, rest, db }: { client: Client; rest: REST; db: Database }) => {
   try {
     loadEvents(client)
-    installCommands(rest)({
-      clientId: process.env.APP_ID || '',
-      guildId: process.env.GUILD_ID || '',
-    })
-    loadCommands(client)
     startApi()
     initDb(db)
   } catch (error) {
