383767a014f803a4b8f8dd605d4dea20515c611e
Roy Scheeren <hi@royscheeren.com>
2023-06-28 12:14:18 +0200

    refactor(sdk): replace axios with fetch
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- packages/sdk/src/lib/http/http.ts
+++ packages/sdk/src/lib/http/http.ts
@@ -1,10 +1,36 @@
-/*}
+/*
  * Copyright (C) 2022, vDL Digital Ventures GmbH <info@vdl.digital>
  *
  * SPDX-License-Identifier: MIT
  */
-import axios, { AxiosInstance } from 'axios'
+import { HTTP, Options } from '../types'
 
-export const http: AxiosInstance = axios.create({
-  timeout: 1000,
-})
+export const fetchWithTimeout = async <TResponse>(resource: string, userOptions: Options = { timeout: 3000 }) => {
+  const defaultOptions: Options = {
+    timeout: 3000,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: null,
+  }
+  const options = { ...defaultOptions, ...userOptions }
+
+  const controller = new AbortController()
+  const id = setTimeout(() => controller.abort(), options.timeout)
+
+  try {
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal,
+    })
+    const data = await response.json()
+    return { data } as TResponse
+  } catch (error) {
+    throw new Error('Fetching failed')
+  } finally {
+    clearTimeout(id)
+  }
+}
+
+export const http: HTTP = fetchWithTimeout
