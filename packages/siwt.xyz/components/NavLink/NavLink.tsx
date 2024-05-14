959043263ddd463634764be9f9da2acdbdb39a18
Roy Scheeren <roy@vdl.digital>
2024-05-14 17:26:57 +0200

    refactor(siwt.xyz): images and responsiveness (#57)
    ---------
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- packages/siwt.xyz/components/NavLink/NavLink.tsx
+++ packages/siwt.xyz/components/NavLink/NavLink.tsx
@@ -17,7 +17,7 @@ export const NavLink: FC<NavLinkProps> = ({ href, children, className = '' }) =>
   <Link
     href={href}
     className={clsx(
-      'inline-block rounded-lg py-1 px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900',
+      'inline-block rounded-lg py-1 md:px-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900',
       className,
     )}
   >
