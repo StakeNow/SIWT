0bf7972ff17201fc45fae568e984b339ff985298
Roy Scheeren <roy@vdl.digital>
2024-05-15 09:54:09 +0200

    fix(siwt.xyz): static output settings (#58)
    * Update static output settings to next 14
    --------------------------------------------
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- packages/siwt.xyz/next.config.js
+++ packages/siwt.xyz/next.config.js
@@ -7,6 +7,8 @@ const { withNx } = require('@nx/next/plugins/with-nx')
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: 'export',
+  distDir: '../../dist/packages/siwt.xyz',
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
